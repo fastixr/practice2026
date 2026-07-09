f4845a694e049c87e1f6aca084594be770813272
fast1x <143862659+fastixr@users.noreply.github.com>
2026-07-09 14:18:47 +0300

    Update C.py

--- day 1/C.py
+++ day 1/C.py
@@ -1,1 +1,9 @@
-
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    res = []
+    for i in range(n):
+        res.append(i + 1)
+        res.append(n + 2*i + 1)
+        res.append(n + 2*i + 2)
+    print(*res)
