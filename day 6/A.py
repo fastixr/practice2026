c2eca91b24cd91938be713d3b50a0cfe1c2f3a96
fast1x <143862659+fastixr@users.noreply.github.com>
2026-07-09 14:29:13 +0300

    Update A.py

--- day 6/A.py
+++ day 6/A.py
@@ -1,1 +1,18 @@
+import sys
+from itertools import combinations
+from functools import reduce
+from operator import xor
 
+def main():
+    data = sys.stdin.read().split()
+    n, k = int(data[0]), int(data[1])
+    a = [int(data[2+i]) for i in range(n)]
+    
+    best = 0
+    for combo in combinations(a, k):
+        val = reduce(xor, combo)
+        if val > best:
+            best = val
+    print(best)
+
+main()
