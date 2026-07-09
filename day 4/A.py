6188101f350735bf7c647989b992672565139f1c
fast1x <143862659+fastixr@users.noreply.github.com>
2026-07-09 14:24:44 +0300

    Update A.py

--- day 4/A.py
+++ day 4/A.py
@@ -1,1 +1,9 @@
-
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    best = 0
+    for i in range(6):
+        for j in range(6 - i):
+            k = 5 - i - j
+            best = max(best, (a+i)*(b+j)*(c+k))
+    print(best)
