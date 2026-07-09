55c63287de0976bade7c320c730ba1a0e81f89e4
fast1x <143862659+fastixr@users.noreply.github.com>
2026-07-09 14:22:32 +0300

    Update A.py

--- day 3/A.py
+++ day 3/A.py
@@ -1,1 +1,30 @@
+import sys
 
+def solve():
+    data = sys.stdin.read().split()
+    n, k, m = int(data[0]), int(data[1]), int(data[2])
+    MOD = 10**9 + 7
+    
+    dangerous = set()
+    for i in range(m):
+        dangerous.add(int(data[3 + i]))
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    
+    window_sum = 1
+    
+    for i in range(1, n + 1):
+        if i in dangerous:
+            dp[i] = 0
+        else:
+            dp[i] = window_sum % MOD
+        
+        window_sum += dp[i]
+        if i >= k:
+            window_sum -= dp[i - k]
+        window_sum %= MOD
+    
+    print(dp[n])
+
+solve()
